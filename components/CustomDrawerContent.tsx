7ac720bc7f9d8da98cfbd6562e545e58f899d46b
UnicornDad8 <unicorndad8@gmail.com>
2025-03-22 07:31:19 -0300

    Style drawer items

--- components/CustomDrawerContent.tsx
+++ components/CustomDrawerContent.tsx
@@ -7,7 +7,7 @@ import { router } from 'expo-router';
 import Feather from '@expo/vector-icons/Feather';
 import items from '@/constants/DrawerItems';
 
-const iconColor = "#334155";
+const iconColor = "#64748B";
 
 function CustomDrawerContent() {
   const insets = useSafeAreaInsets();
@@ -15,26 +15,31 @@ function CustomDrawerContent() {
   return (
     <DrawerContentScrollView style={{ flex: 1, paddingTop: insets.top }}>
       <ScrollView style={{ flex: 1 }}>
-        <View className="flex items-center justify-center py-8 px-6">
-          <View>
+        <View className="flex items-start justify-center" style={{ paddingHorizontal: 15 }}>
+          <View className='overflow-hidden'>
             <Image
               source={require('../assets/images/user-photo.jpg')}
-              style={{ width: 200, height: 200 }}
+              style={{ width: 80, height: 80, borderRadius: 40 }}
             />
           </View>
+          <View style={{ marginTop: 6}}>
+            <Text className='font-bold text-xl' style={{ color: "#020617"}}>Sophia Rose</Text>
+            <Text className='text-sm' style={{ color: "#94A3B8" }}>UX/UI Designer</Text>
+          </View>
         </View>
+        <View style={{ height: 1, backgroundColor: "#E2E8F0", marginVertical: 20}} /> 
         <View>
           {items.map((item) => {
             return (
               <TouchableOpacity
                 key={item.label}
-                className='bg-red-200'
+                style={{ display: "flex", flexDirection: "row", paddingHorizontal: 15, paddingVertical: 15, alignItems: "center" }}
                 onPress={() => {
                   router.push(item.url);
                 }}
               >
-              <Feather name={item.icon} size={24} color={iconColor} />
-              <Text>{item.label}</Text>
+              <Feather name={item.icon} size={24} color={iconColor} style={{ marginRight: 10 }} />
+              <Text style={{ color: "#020617", fontWeight: 500 }} >{item.label}</Text>
             </TouchableOpacity>
             )
           })}
